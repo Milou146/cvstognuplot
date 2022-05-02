2f6d7398c9290ec45c0f9a8552aaebe49f1e912a
Milou146 <57072450+Milou146@users.noreply.github.com>
2022-05-03 01:42:54 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+Compiler : g++ main.cpp -o main
+Executer : ./main nomfichier.cvs
+Exemple : ./main test-PMR_simple.csv
+Sortie : output.in et ouput.dat
+output.in : fichier d'entrée de gnu plot
+output.dat le fichier data que peut lire gnuplot
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <fstream> 
+
+using namespace std;
+int main(int argc,char * argv[]){
+    //Ouverture du fichier test-PMR_simple.csv
+    ifstream in_file;
+    const char *filename = argv[1];
+    string str(filename);
+    in_file.open(filename);
+
+    if(in_file.is_open()){
+        ofstream out_file;
+        // Si fichier ouvert, affichage des lignes avec numero d'occurrence ajoute
+        cout << filename <<" is opened"<<endl;
+        string line;
+        int occurrence = 1;
+        //lecture ligne a ligne jusque fin du fichier
+        out_file.open("output.dat"); 
+        if(out_file.is_open()){
+            while(!in_file.eof() && line != "\"traffic\",,"){
+                getline(in_file, line);
+                if(line == "\"Node\",," || line == "\"traffic\",,"){ // on s'arrête juste avant
+                    cout << occurrence++ << " : type 1 : " << line << endl;
+                }
+                else{
+                    cout << occurrence++ << " : type 6 : " << line << endl;
+                    line.replace(0,7,"",0);
+                    if(line.substr(0,3) == "   "){
+                        line.replace(0,3,"",0);
+                    }
+                    else{
+                        line.replace(0,1,"",0);
+                    }
+                    line.replace(2,1,"   ",3);
+                    out_file << line << endl;
+                }
+            }
+        }
+        //fermeture du fichier
+        out_file.close();
+        in_file.close();
+        // fichier d'entrée pour gnuplot
+        out_file.open("output.in"); 
+        out_file << "plot 'output.dat' with lines" << endl;
+        out_file.close();
+    }else{
+        cout << filename <<" is not opened"<<endl;
+    }
+  return 0;
+}
